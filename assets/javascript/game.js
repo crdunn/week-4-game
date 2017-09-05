53d17e88e379a3779e7918941d82dcaca5a7a5d4
Charles Dunn <charlesdunn@Charless-MacBook-Pro.local>
2017-09-05 11:58:59 -0400

    refining

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,10 +2,10 @@
 var counter = 0;
 var goalNumber
 var options = [1,2,3,4,5,6,7,8,9,10,11,12];
-var rand1
-var rand2
-var rand3
-var rand4
+var rand1 = 0
+var rand2 = 0
+var rand3 = 0
+var rand4 = 0
 var randArray
 var win = 0
 var loss = 0
@@ -13,6 +13,7 @@ var audioBegin = document.createElement("audio");
     audioBegin.setAttribute("src", "Assets/images/pylons.mp3");
 var audioLoss = document.createElement("audio");
     audioLoss.setAttribute("src", "Assets/images/minerals.mp3");
+var lossState = false;
 
 
 function winCounter() {
@@ -22,8 +23,9 @@ function winCounter() {
 	alert ("VICTORY!");
 	win += 1;
 	$("#win").text(win);
-	} else if (counter >= goalNumber) {
+	} else if (counter >= goalNumber && lossState === false) {
 	audioLoss.play();
+	lossState = true;
 	loss +=1;
 	$("#loss").text(loss);
 	};
@@ -33,13 +35,14 @@ function winCounter() {
 $("#goal").on("click", function() {
 	goalNumber = Math.floor(Math.random()*101)+19;
 	counter = 0
+	lossState = false
     audioBegin.play();
 	$("#numberToUse").text(goalNumber);
 	$("#current").text(counter);
-	rand1 = Math.floor(Math.random()*12);
-	rand2 = Math.floor(Math.random()*12);
-	rand3 = Math.floor(Math.random()*12);
-	rand4 = Math.floor(Math.random()*12);
+	rand1 = Math.floor(Math.random()*11)+1;
+	rand2 = Math.floor(Math.random()*11)+1;
+	rand3 = Math.floor(Math.random()*11)+1;
+	rand4 = Math.floor(Math.random()*11)+1;
 	randArray = [rand1,rand2,rand3,rand4];
 	console.log (randArray);
 
